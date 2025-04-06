ffd405d9b58c93773ff18abecc8150d802d7664e
JackBaraka <jack.baraka15894@gmail.com>
2025-04-06 14:13:56 +0300

    initial

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+
+API_KEY = "1e2b3f32571c5fcaa080574a7efb7102"
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+FORECAST_URL = "https://api.openweathermap.org/data/2.5/forecast"
